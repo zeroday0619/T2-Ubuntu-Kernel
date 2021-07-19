ddde8e4c0bc16a0f957dc4373053b2fc5b23d8e2
AdityaGarg8 <85610623+AdityaGarg8@users.noreply.github.com>
2021-07-19 10:21:12 +0530

    Update patch_driver.sh

--- patch_driver.sh
+++ patch_driver.sh
@@ -8,7 +8,7 @@ BUILD_PATH=/tmp/build-kernel
 # Patches
 APPLE_SMC_DRIVER_GIT_URL=https://github.com/jamlam/mbp-16.1-linux-wifi.git
 APPLE_SMC_DRIVER_BRANCH_NAME=main
-APPLE_SMC_DRIVER_COMMIT_HASH=4c8b393ed7a874e3d9e44a2a467c1b7c74af1260
+APPLE_SMC_DRIVER_COMMIT_HASH=c0d492b15c227d3dfc06fbcad3335e1a5aa00ca4
 ## BCE
 #APPLE_BCE_DRIVER_GIT_URL=https://github.com/aunali1/mbp2018-bridge-drv.git
 #APPLE_BCE_DRIVER_BRANCH_NAME=aur
