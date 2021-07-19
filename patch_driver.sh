5dd9317e4333d039b4e5492e7e638b9a81c83562
AdityaGarg8 <85610623+AdityaGarg8@users.noreply.github.com>
2021-07-19 21:28:33 +0530

    Update patch_driver.sh

--- patch_driver.sh
+++ patch_driver.sh
@@ -6,9 +6,9 @@ BUILD_PATH=/tmp/build-kernel
 
 ### Apple T2 drivers commit hashes
 # Patches
-APPLE_SMC_DRIVER_GIT_URL=https://github.com/jamlam/mbp-16.1-linux-wifi.git
+APPLE_SMC_DRIVER_GIT_URL=https://github.com/AdityaGarg8/test.git
 APPLE_SMC_DRIVER_BRANCH_NAME=main
-APPLE_SMC_DRIVER_COMMIT_HASH=c0d492b15c227d3dfc06fbcad3335e1a5aa00ca4
+APPLE_SMC_DRIVER_COMMIT_HASH=8ddf3a0b5369252a929c40ee79b4603d8af6d4a1
 ## BCE
 #APPLE_BCE_DRIVER_GIT_URL=https://github.com/aunali1/mbp2018-bridge-drv.git
 #APPLE_BCE_DRIVER_BRANCH_NAME=aur
