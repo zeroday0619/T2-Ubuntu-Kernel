a6747f115398f7384c5423eca00cecc8d779de0c
AdityaGarg8 <85610623+AdityaGarg8@users.noreply.github.com>
2021-10-07 16:59:22 +0530

    5.13-14.14-HWE-exFAT

--- patch_driver.sh
+++ patch_driver.sh
@@ -6,9 +6,9 @@ BUILD_PATH=/tmp/build-kernel
 
 ### Apple T2 drivers commit hashes
 # Patches
-APPLE_SMC_DRIVER_GIT_URL=https://github.com/jamlam/mbp-16.1-linux-wifi.git
-APPLE_SMC_DRIVER_BRANCH_NAME=main
-APPLE_SMC_DRIVER_COMMIT_HASH=a30e7fbe00f2f1a54f4157b9092fe7a546b85dfc
+APPLE_SMC_DRIVER_GIT_URL=https://github.com/AdityaGarg8/mbp-16.1-linux-wifi.git
+APPLE_SMC_DRIVER_BRANCH_NAME=5.13
+APPLE_SMC_DRIVER_COMMIT_HASH=d470620be6e9a9de6e3ba7886099b5aa8b4a3ba5
 ## BCE
 #APPLE_BCE_DRIVER_GIT_URL=https://github.com/aunali1/mbp2018-bridge-drv.git
 #APPLE_BCE_DRIVER_BRANCH_NAME=aur
