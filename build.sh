5badaa0010204c2f261a0c0b487a54be3858df9c
AdityaGarg8 <85610623+AdityaGarg8@users.noreply.github.com>
2021-11-13 12:54:44 +0530

    5.15.2 - recompile

--- build.sh
+++ build.sh
@@ -19,17 +19,6 @@ grep 'model name' /proc/cpuinfo | uniq
 
 get_next_version () {
   echo $PKGREL
-  #curl -s https://mbp-ubuntu-kernel.herokuapp.com/ -L | grep "linux-image-${KERNEL_VERSION}-${1}" > /dev/null
-  #OLD_BUILD_EXIST=$?
-  #if test $OLD_BUILD_EXIST -eq 0
-  #then
-  #  LATEST_BUILD=$(curl -s https://mbp-ubuntu-kernel.herokuapp.com/ -L | grep "linux-image-${KERNEL_VERSION}-${1}" |
-  #    grep a | cut -d'>' -f2 | cut -d'<' -f1 |
-  #    sort -r | head -n 1 | cut -d'-' -f6 | cut -d'_' -f1)
-  #else
-  #  LATEST_BUILD=0
-  #fi
-  #echo "$((LATEST_BUILD+1))"
 }
 
 ### Clean up
