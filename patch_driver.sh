29e5d5d2d795abd286f0da06c3321c40616375cf
Aditya Garg <gargaditya08@live.com>
2022-02-02 17:48:42 +0530

    5.16.5 with touchbar fix

--- patch_driver.sh
+++ patch_driver.sh
@@ -8,7 +8,7 @@ BUILD_PATH=/tmp/build-kernel
 # Patches
 APPLE_SMC_DRIVER_GIT_URL=https://github.com/Redecorating/mbp-16.1-linux-wifi.git
 APPLE_SMC_DRIVER_BRANCH_NAME=main
-APPLE_SMC_DRIVER_COMMIT_HASH=d836948553a4b261d7ae1ae77305f9b86ffbaaf5
+APPLE_SMC_DRIVER_COMMIT_HASH=c1dc3beb31f1c5b05f0f2319472d4323b19dc3a7
 ## BCE
 #APPLE_BCE_DRIVER_GIT_URL=https://github.com/aunali1/mbp2018-bridge-drv.git
 #APPLE_BCE_DRIVER_BRANCH_NAME=aur
@@ -27,7 +27,6 @@ git clone --single-branch --branch ${APPLE_SMC_DRIVER_BRANCH_NAME} ${APPLE_SMC_D
   "${BUILD_PATH}/linux-mbp-arch"
 cd "${BUILD_PATH}/linux-mbp-arch" || exit
 git checkout ${APPLE_SMC_DRIVER_COMMIT_HASH}
-rm 0101*
 
 while IFS= read -r file; do
   echo "==> Adding ${file}"
