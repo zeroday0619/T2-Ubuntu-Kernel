303cf2885e806c8e05fbdfc24afa855c05a00d1d
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2022-03-18 14:28:17 +0530

    5.15.0-23.23

--- patch_driver.sh
+++ patch_driver.sh
@@ -22,13 +22,19 @@ rm -rf "${BUILD_PATH}"
 mkdir -p "${BUILD_PATH}"
 cd "${BUILD_PATH}" || exit
 
+### Get the wifi patches seperately
+git clone --single-branch --branch main https://github.com/AdityaGarg8/linux-t2-patches \
+  "${BUILD_PATH}/linux-mbp-arch-wifi"
 ### AppleSMC and BT aunali fixes
 git clone --single-branch --branch ${APPLE_SMC_DRIVER_BRANCH_NAME} ${APPLE_SMC_DRIVER_GIT_URL} \
   "${BUILD_PATH}/linux-mbp-arch"
 cd "${BUILD_PATH}/linux-mbp-arch" || exit
 git checkout ${APPLE_SMC_DRIVER_COMMIT_HASH}
 rm 9001*
+rm 2001*
+rm 80*
 rm intel-lpss.patch
+cp "${BUILD_PATH}/linux-mbp-arch-wifi"/80* "${BUILD_PATH}/linux-mbp-arch"
 
 while IFS= read -r file; do
   echo "==> Adding ${file}"
